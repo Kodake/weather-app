a3b1cf7a7e1a984c004f24b69a756cb2978fbd77
Kodake <kodake@hotmail.es>
2021-11-28 11:23:14 -0400

    Solving the change color issue depending on the temperature

--- App.js
+++ App.js
@@ -4,56 +4,61 @@ import Formulario from './components/Formulario';
 import Clima from './components/Clima';
 
 const App = () => {
-
-  const [busqueda, guardarBusqueda] = useState({
+  const [busqueda, setBusqueda] = useState({
     ciudad: '',
-    pais: ''
+    pais: '',
   });
-
-  const [consultar, guardarConsultar] = useState(false);
-  const [resultado, guardarResultado] = useState({});
-
+  const [consultar, setConsultar] = useState(false);
   const { ciudad, pais } = busqueda;
-
+  const [resultado, setResultado] = useState({});
+  const [bgColor, setBgColor] = useState('rgb(71, 149, 212)');
+  const mostrarAlerta = () => {
+    Alert.alert('Error', 'No hay resultados, intenta con otra ciudad o país', [
+      { text: 'OK' },
+    ]);
+  };
+  const ocultarTeclado = () => {
+    Keyboard.dismiss();
+  };
+  const bgColorApp = {
+    backgroundColor: bgColor,
+  };
   useEffect(() => {
     const consultarClima = async () => {
       if (consultar) {
-        console.log('Consultando la API...');
-        const API_KEY = `9e98f86b6c35e2b3a50d7cdcf47cac37`;
-        const API_URL = `api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${API_KEY}`;
-
+        const appId = `9e98f86b6c35e2b3a50d7cdcf47cac37`;
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
         try {
-          const response = await fetch(API_URL);
-          const result = await response.json();
-          guardarResultado(result);
-          guardarConsultar(false);
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+          setResultado(resultado);
+          setConsultar(false);
+          const kelvin = 273.15;
+          const { main } = resultado;
+          const actual = main.temp - kelvin;
+          // Modifica los colores según temperatura
+          if (actual < 15) {
+            setBgColor('rgb(105, 108, 149)');
+          } else if (actual >= 15 && actual < 25) {
+            setBgColor('rgb(71, 149, 212)');
+          } else {
+            setBgColor('rgb(178, 28, 61)');
+          }
         } catch (error) {
           mostrarAlerta();
         }
       }
-    }
+    };
     consultarClima();
   }, [consultar]);
-
-  const mostrarAlerta = () => {
-    Alert.alert(
-      'Error',
-      'no hay resultados, intenta con otra ciudad o país.',
-      [{ text: 'Ok' }]
-    )
-  }
-
-  const ocultarTeclado = () => {
-    Keyboard.dismiss();
-  }
-
+  
   return (
     <>
       <TouchableWithoutFeedback onPress={() => ocultarTeclado()}>
-        <View style={styles.app}>
+        <View style={[styles.app, bgColorApp]}>
           <View style={styles.contenido}>
-            <Clima resultado={resultado}/>
-            <Formulario busqueda={busqueda} guardarBusqueda={guardarBusqueda} guardarConsultar={guardarConsultar} />
+            <Clima resultado={resultado} />
+            <Formulario busqueda={busqueda} setBusqueda={setBusqueda} setConsultar={setConsultar} />
           </View>
         </View>
       </TouchableWithoutFeedback>
@@ -64,12 +69,11 @@ const App = () => {
 const styles = StyleSheet.create({
   app: {
     flex: 1,
-    backgroundColor: '#0F2C67',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   contenido: {
-    marginHorizontal: '2.5%'
-  }
+    marginHorizontal: '2.5%',
+  },
 });
 
 export default App;
--- components/Formulario.js
+++ components/Formulario.js
@@ -1,106 +1,95 @@
+import { Picker } from '@react-native-picker/picker';
 import React, { useState } from 'react';
 import { Text, View, TextInput, StyleSheet, TouchableWithoutFeedback, Animated, Alert } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
 
-const Formulario = ({ busqueda, guardarBusqueda, guardarConsultar }) => {
-
-    const { pais, ciudad } = busqueda;
-
-    const [animacionBoton] = useState(new Animated.Value(1));
-
-    const consultarClima = () => {
-        if (pais.trim() === '' || ciudad.trim() === '') {
-            mostrarAlerta();
-            return false;
-        }
-
-        guardarConsultar(true);
+const Formulario = ({ busqueda, setBusqueda, setConsultar }) => {
+  const { pais, ciudad } = busqueda;
+  const [animacionBtn] = useState(new Animated.Value(1));
+  const mostrarAlerta = () => {
+    Alert.alert('Error', 'Agrega una ciudad y un país para la búsqueda', [
+      { text: 'Entendido' },
+    ]);
+  };
+  const animacionEntrada = () => {
+    Animated.spring(animacionBtn, {
+      toValue: 0.75,
+      useNativeDriver: true,
+    }).start();
+  };
+  const animacionSalida = () => {
+    Animated.spring(animacionBtn, {
+      toValue: 1,
+      friction: 4,
+      tension: 30,
+      useNativeDriver: true,
+    }).start();
+  };
+  const estiloAnimacion = {
+    transform: [{ scale: animacionBtn }],
+  };
+  const consultarClima = () => {
+    if (pais.trim() === '' || ciudad.trim() === '') {
+      mostrarAlerta();
+      return;
     }
-
-    const mostrarAlerta = () => {
-        Alert.alert(
-            'Error',
-            'Agrega una ciudad y país a la búsqueda.',
-            [{ text: 'Entendido...' }]
-        )
-    }
-
-    const animacionEntrada = () => {
-        Animated.spring(animacionBoton, {
-            toValue: .75
-        }).start();
-    }
-
-    const animacionSalida = () => {
-        Animated.spring(animacionBoton, {
-            toValue: 1,
-            friction: 4,
-            tension: 100
-        }).start();
-    }
-
-    const estiloAnimacion = {
-        transform: [{ scale: animacionBoton }]
-    }
-
-    return (
-        <View style={StyleSheet.formulario}>
-            <View>
-                <TextInput
-                    value={ciudad}
-                    style={styles.input}
-                    onChange={city => guardarBusqueda({ ...busqueda, city })}
-                    placeholder='Ciudad'
-                    placeholderTextColor='#666'
-                />
-            </View>
-
-            <View>
-                <Picker
-                    selectedValue={country => guardarBusqueda({ ...busqueda, country })}
-                    itemStyle={{ height: 120, backgroundColor: '#FFF' }}>
-                    <Picker.Item label='--Seleccione un país--' value='' />
-                    <Picker.Item label='México' value='MX' />
-                    <Picker.Item label='Argentina' value='AR' />
-                </Picker>
-            </View>
-
-            <TouchableWithoutFeedback
-                onPressIn={() => animacionEntrada()}
-                onPressOut={() => animacionSalida()}
-                onPress={() => consultarClima()}
-            >
-                <Animated.View style={[styles.btnBuscar, estiloAnimacion]}>
-                    <Text style={styles.textoBuscar}>Buscar Clima</Text>
-                </Animated.View>
-            </TouchableWithoutFeedback>
+    // Consultar la API
+    setConsultar(true);
+  };
+  return (
+    <>
+      <View>
+        <View>
+          <TextInput value={ciudad} style={styles.input} onChangeText={ciudad => setBusqueda({ ...busqueda, ciudad })} placeholder="Ciudad" placeholderTextColor="#666"
+          />
         </View>
-
-    )
-}
+        <View>
+          <Picker selectedValue={pais} style={styles.selector} onValueChange={pais => setBusqueda({ ...busqueda, pais })}>
+            <Picker.Item label="-- Seleccione un país --" value="" />
+            <Picker.Item label="Estados Unidos" value="US" />
+            <Picker.Item label="México" value="MX" />
+            <Picker.Item label="Argentina" value="AR" />
+            <Picker.Item label="Colombia" value="CO" />
+            <Picker.Item label="Costa Rica" value="CR" />
+            <Picker.Item label="España" value="ES" />
+            <Picker.Item label="Ecuador" value="EC" />
+          </Picker>
+        </View>
+        <TouchableWithoutFeedback onPressIn={() => animacionEntrada()} onPressOut={() => animacionSalida()} onPress={() => consultarClima()}>
+          <Animated.View style={[styles.btnBuscar, estiloAnimacion]}>
+            <Text style={styles.txtBuscar}>Buscar clima</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      </View>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-    input: {
-        padding: 10,
-        height: 50,
-        backgroundColor: '#FFF',
-        fontSize: 20,
-        marginBottom: 20,
-        textAlign: 'center'
-    },
-    btnBuscar: {
-        marginTop: 15,
-        padding: 10,
-        justifyContent: 'center',
-        backgroundColor: '#000'
-    },
-    textoBuscar: {
-        color: '#FFF',
-        fontWeight: 'bold',
-        textTransform: 'uppercase',
-        textAlign: 'center',
-        fontSize: 18
-    }
-})
+  input: {
+    padding: 10,
+    height: 50,
+    backgroundColor: '#FFF',
+    fontSize: 20,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  btnBuscar: {
+    marginTop: 50,
+    backgroundColor: '#000',
+    padding: 10,
+    justifyContent: 'center',
+  },
+  txtBuscar: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  selector: {
+    height: 50,
+    backgroundColor: '#FFF',
+  }
+});
 
 export default Formulario;
